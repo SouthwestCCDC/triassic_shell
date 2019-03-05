689e9ebcaafba278b1af276b96efd137a38f2836
Brady Deetz <bdeetz@gmail.com>
2019-03-05 14:45:40 -0600

    updated consensus server added resync functionality fixed other issues

--- data_model.py
+++ data_model.py
@@ -1,71 +1,11 @@
 import json
+import time
+import sys
 
 import persistent
 import ZODB, ZODB.FileStorage, transaction
 import BTrees.OOBTree
 from triassic_consensus.client import DistributedDict
-
-
-class FenceSegment(persistent.Persistent):
-    def __init__(self, id, dinosaur_name):
-        self.id = id
-        self.dinosaur = dinosaur_name
-
-        global dist_dict
-        self.dist_dict = dist_dict
-
-        # TODO: Changing the following need to set self._p_changed=True:
-        self._state = 1.0
-        self._enabled = True
-
-        if self.dist_dict is not None:
-            self.dist_dict['%d_dino_name' % id] = dinosaur_name
-            self.dist_dict['%d_state' % id] = self._state
-            self.dist_dict['%d_enabled' % id] = self._enabled
-
-    @property
-    def state(self):
-        # if self.dist_dict is not None:
-        #     self._state = self.dist_dict['%d_state' % self.id]
-
-        return self._state
-
-    @state.setter
-    def state(self, value):
-        if self.dist_dict is not None:
-            self.dist_dict['%d_state' % self.id] = value
-
-        self._state = value
-        self._p_changed = True
-
-
-    @property
-    def enabled(self):
-        # if self.dist_dict is not None:
-        #     self._enabled = self.dist_dict['%d_enabled' % self.id]
-
-        return self._enabled
-
-    @enabled.setter
-    def enabled(self, value):
-        if self.dist_dict is not None:
-            self.dist_dict['%d_enabled' % self.id] = value
-
-        self._enabled = value
-        self._p_changed = True
-
-    def reset_state(self):
-        self.state = 1.0
-
-    def fence_status(self):
-        if not self.enabled:
-            return 'pwroff'
-        if self.state < 0.25:
-            return 'failed'
-        elif self.state < 0.5:
-            return 'degrad'
-        else:
-            return 'ok'
 
 db = None
 db_path = None
@@ -74,6 +14,113 @@ config = None
 
 with open('./triassic_shell.conf', 'r') as fp:
     config = json.load(fp)
+
+
+class FenceSegment(persistent.Persistent):
+    def __init__(self, id, dinosaur_name):
+        self.id = id
+        self.dinosaur = dinosaur_name
+
+        # TODO: Changing the following need to set self._p_changed=True:
+        self._state = 1.0
+        self._enabled = True
+
+        global dist_dict
+
+        if dist_dict is not None:
+            dist_dict['%d_dino_name' % id] = dinosaur_name
+            self._state = dist_dict['%d_state' % id]
+            self._enabled = dist_dict['%d_enabled' % id]
+
+
+    @property
+    def state(self):
+        global dist_dict
+        if dist_dict is not None:
+            conn = get_db_conn()
+            #print('setting zodb %d_state' % self.id)
+            conn.root.fence_segments[self.id].state = dist_dict['%d_state' % self.id]
+            transaction.commit()
+            conn.close()
+
+            #print(dist_dict['%d_last_updated' % self.id])
+        else:
+           time.sleep(.3)
+
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        global dist_dict
+        if dist_dict is not None:
+            #print('getting %d_state' % self.id)
+            dist_dict['%d_state' % self.id] = value
+        else:
+            time.sleep(.3)
+
+        self._state = value
+        self._p_changed = True
+
+
+    @property
+    def enabled(self):
+        global dist_dict
+        if dist_dict is not None:
+            conn = get_db_conn()
+            #print('syncing zodb %d_enabled to dist_dict %s' % (self.id, dist_dict['%d_enabled' % self.id]))
+            conn.root.fence_segments[self.id].enabled = dist_dict['%d_enabled' % self.id]
+            #print('zodb set')
+            transaction.commit()
+            #print('zodb commit')
+            conn.close()
+            #print('zodb close')
+        else:
+            time.sleep(.3)
+
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value):
+        global dist_dict
+        if dist_dict is not None:
+            #print('setting dist_dict and zodb %d_enabled to %s' % (self.id, value))
+            dist_dict['%d_enabled' % self.id] = value
+            #print('set complete')
+        else:
+            time.sleep(.3)
+
+        self._enabled = value
+        #print('_enabled set')
+        self._p_changed = True
+        #print('_p_changed set')
+
+    def reset_state(self):
+        self.state = 1.0
+
+    def fence_status(self):
+        #print('getting enabled')
+        if not self.enabled:
+            #print('enabled')
+            sys.stdout.flush()
+            return 'pwroff'
+
+        #print('getting state')
+        if self.state < 0.25:
+            #print('state<.25')
+            return 'failed'
+
+        #print('getting state')
+        if self.state < 0.5:
+            #print('state<.5')
+            return 'degrad'
+        else:
+            #print('ok')
+            return 'ok'
+
+    def resync(self):
+        global dist_dict
+        if dist_dict is not None:
+            dist_dict['%d_task_queued' % self.id] = 'df5ac'
 
 def get_db_conn():
     global db
