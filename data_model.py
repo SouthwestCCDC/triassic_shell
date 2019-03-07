67092fe9ae03c1c67f548217c252661b8d9f9431
Brady Deetz <bdeetz@gmail.com>
2019-03-07 15:45:08 -0600

    fixing unhandled exceptions

--- data_model.py
+++ data_model.py
@@ -46,67 +46,59 @@ class FenceSegment(persistent.Persistent):
                 self._enabled = dist_dict['%d_enabled' % id]
 
 
+    def get_dist_value(self, key):
+        while True:
+            try:
+                return dist_dict[key]
+                break
+            except:
+                dist_dict_reconnect()
+                time.sleep(1)
+
+    def set_dist_value(self, key, value):
+        while True:
+            try:
+                dist_dict[key] = value
+                break
+            except:
+                dist_dict_reconnect()
+                time.sleep(1)
+
     @property
     def state(self):
         global dist_dict
         if dist_dict is not None:
             conn = get_db_conn()
-            #print('setting zodb %d_state' % self.id)
-            try:
-                time_delta_seconds = 900
-                start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
-                end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
-                node_last_update = datetime.datetime.strptime(dist_dict['%d_last_updated' % self.id], '%Y-%m-%d %H:%M:%S.%f')
 
-                if not self.time_in_range(start_time, end_time, node_last_update):
-                    dist_dict['%d_state' % self.id] = 0
+            time_delta_seconds = 900
+            start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
+            end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
 
-                conn.root.fence_segments[self.id].state = dist_dict['%d_state' % self.id]
-            except:
-                #print('reconnecting')
-                dist_dict_reconnect()
+            node_last_update = datetime.datetime.strptime(self.get_dist_value('%d_last_updated' % self.id), '%Y-%m-%d %H:%M:%S.%f')
 
-                time_delta_seconds = 900
-                start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
-                end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
-                node_last_update = datetime.datetime.strptime(dist_dict['%d_last_updated' % self.id], '%Y-%m-%d %H:%M:%S.%f')
+            if not self.time_in_range(start_time, end_time, node_last_update):
+                self.set_dist_value('%d_state' % self.id, 0)
 
-                if not self.time_in_range(start_time, end_time, node_last_update):
-                    dist_dict['%d_state' % self.id] = 0
+            conn.root.fence_segments[self.id].state = dist_dict['%d_state' % self.id]
+            conn.root.fence_segments[self.id].state = self.get_dist_value('%d_state' % self.id)
 
-                conn.root.fence_segments[self.id].state = dist_dict['%d_state' % self.id]
-
-            #print('commiting on state getter')
             transaction.commit()
-            #print('closing on state getter')
             conn.close()
-
-            #print(dist_dict['%d_last_updated' % self.id])
         else:
-           time.sleep(.3)
+            time.sleep(.3)
 
-        #print('returning from state getter')
         return self._state
 
     @state.setter
     def state(self, value):
         global dist_dict
         if dist_dict is not None:
-            #print('getting %d_state' % self.id)
-            try:
-                dist_dict['%d_state' % self.id] = value
-            except:
-                dist_dict_reconnect()
-                dist_dict['%d_state' % self.id] = value
-
-            #print('%d_state set')
-
+            self.set_dist_value('%d_state' % self.id, value)
         else:
             time.sleep(.3)
 
         self._state = value
         self._p_changed = True
-        #print('returning')
 
 
     @property
@@ -114,18 +106,11 @@ class FenceSegment(persistent.Persistent):
         global dist_dict
         if dist_dict is not None:
             conn = get_db_conn()
-            #print('syncing zodb %d_enabled to dist_dict %s' % (self.id, dist_dict['%d_enabled' % self.id]))
-            try:
-                conn.root.fence_segments[self.id].enabled = dist_dict['%d_enabled' % self.id]
-            except:
-                dist_dict_reconnect()
-                conn.root.fence_segments[self.id].enabled = dist_dict['%d_enabled' % self.id]
 
-            #print('zodb set')
+            conn.root.fence_segments[self.id].enabled = self.get_dist_value('%d_enabled' % self.id)
+
             transaction.commit()
-            #print('zodb commit')
             conn.close()
-            #print('zodb close')
         else:
             time.sleep(.3)
 
@@ -135,69 +120,40 @@ class FenceSegment(persistent.Persistent):
     def enabled(self, value):
         global dist_dict
         if dist_dict is not None:
-            #print('setting dist_dict and zodb %d_enabled to %s' % (self.id, value))
-            try:
-                dist_dict['%d_enabled' % self.id] = value
-            except:
-                dist_dict_reconnect()
-                dist_dict['%d_enabled' % self.id] = value
-
-            #print('set complete')
+            self.set_dist_value('%d_enabled' % self.id, value)
         else:
             time.sleep(.3)
 
         self._enabled = value
-        #print('_enabled set')
         self._p_changed = True
-        #print('_p_changed set')
 
     def fence_status(self):
-        #print('getting enabled')
         if not self.enabled:
-            #print('enabled')
             return 'pwroff'
 
-        #print('getting state')
         if self.state < 0.25:
-            #print('state<.25')
             return 'failed'
-
-        #print('getting state')
-        if self.state < 1.0:
-            #print('state<.5')
+        elif self.state < 1.0:
             return 'degrad'
         else:
-            #print('ok')
             return 'ok'
 
     def resync(self):
         global dist_dict
         if dist_dict is not None:
-            try:
-                time_delta_seconds = 900
-                start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
-                end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
-                node_last_update = datetime.datetime.strptime(dist_dict['%d_last_updated' % self.id], '%Y-%m-%d %H:%M:%S.%f')
+            time_delta_seconds = 900
+            start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
+            end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
+            node_last_update = datetime.datetime.strptime(self.get_dist_value('%d_last_updated' % self.id),
+                                                          '%Y-%m-%d %H:%M:%S.%f')
 
-                if not self.time_in_range(start_time, end_time, node_last_update):
-                    dist_dict['%d_state' % self.id] = 0
-                else:
-                    dist_dict['%d_task_queued' % self.id] = 'df5ac'
-                    time.sleep(5)
-                    dist_dict['%d_state' % self.id] = 1.0
-            except:
-                dist_dict_reconnect()
-                time_delta_seconds = 900
-                start_time = datetime.datetime.now() - datetime.timedelta(seconds=time_delta_seconds)
-                end_time = datetime.datetime.now() + datetime.timedelta(seconds=time_delta_seconds)
-                node_last_update = datetime.datetime.strptime(dist_dict['%d_last_updated' % self.id], '%Y-%m-%d %H:%M:%S.%f')
+            if not self.time_in_range(start_time, end_time, node_last_update):
+                self.set_dist_value('%d_state' % self.id, 0)
+            else:
+                self.set_dist_value('%d_task_queued' % self.id, 'df5ac')
+                time.sleep(5)
+                self.set_dist_value('%d_state' % self.id, 1.0)
 
-                if not self.time_in_range(start_time, end_time, node_last_update):
-                    dist_dict['%d_state' % self.id] = 0
-                else:
-                    dist_dict['%d_task_queued' % self.id] = 'df5ac'
-                    time.sleep(5)
-                    dist_dict['%d_state' % self.id] = 1.0
 
     def time_in_range(self, start, end, x):
         """Return true if x is in the range [start, end]"""
@@ -261,7 +217,7 @@ def init_db(filepath):
         pass
 
     conn.root.fence_segments = BTrees.OOBTree.BTree()
-    for id in range(0x10000+10111, 0xfffff, 10111): # 97x2 elements I think
+    for id in range(0x10000+10111, 0xfffff, 10111): # 97 elements
         if id % 5 == 0:
             dino_name = 'velociraptor'
         elif id % 4 == 0:
